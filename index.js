2ab033b9d6fe0a8d9d448b8f3011e4caeeffdf64
jongleberry <me@jongleberry.com>
2016-05-21 00:53:24 -0700

    travis: support node@4

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 'use strict'
 
-const { randomBytes, createHash } = require('crypto')
 const stringify = require('json-stringify-safe')
-const { isPlainObject } = require('lodash')
 const secp256k1 = require('secp256k1')
 const sort = require('sorted-object')
+const crypto = require('crypto')
+const _ = require('lodash')
 
 exports.createPrivateKey = () => {
   // generate privKey
   let privKey
   do {
-    privKey = randomBytes(32)
+    privKey = crypto.randomBytes(32)
   } while (!secp256k1.privateKeyVerify(privKey))
   return privKey
 }
@@ -20,9 +20,9 @@ exports.createPublicKey = privateKey => {
 }
 
 exports.createMessage = data => {
-  if (isPlainObject(data)) data = stringify(sort(data))
+  if (_.isPlainObject(data)) data = stringify(sort(data))
   else if (Array.isArray(data)) data = stringify(data)
-  return createHash('sha256').update(data).digest()
+  return crypto.createHash('sha256').update(data).digest()
 }
 
 exports.signMessage = (message, privateKey) => {
